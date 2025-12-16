1a7faa51c4317e2aa52dc2a3ba8cda3a7ce8206c
igkonov <igkonov@yandex.ru>
2025-12-16 18:49:39 +0300

    3

--- 1.py
+++ 1.py
@@ -2,20 +2,16 @@ import sounddevice as sd
 import vosk
 import json
 import queue
-import os
-from openai import OpenAI
 
 device_m = 5                                                  # Индекс аудиоустройства (микрофон)
-model = vosk.Model(r"model_stt/vosk-model-small-ru-0.22/vosk-model-small-ru-0.22")      # Модель нейросети
+model = vosk.Model("vosk-model-small-ru-0.22")      # Модель нейросети
 samplerate = 44100                                            # Частота дискретизации микрофона
 q = queue.Queue()                                             # Потоковый контейнер
-print(sd.query_devices())
+
 
 def q_callback(indata, frames, time, status):
     q.put(bytes(indata))
 
-messages = []
-client = OpenAI(api_key='sk-e7d8554ac3d34cf8b4f7f51912ed432d', base_url="https://api.deepseek.com")
 
 def voce_listen():
     with sd.RawInputStream(callback=q_callback, channels=2, samplerate=samplerate, device=device_m, dtype='int16'):
@@ -27,17 +23,6 @@ def voce_listen():
                 res = json.loads(rec.Result())["text"]
                 if res:
                     print(f"Фраза целиком: {res}")
-                    messages.append(res)
-
-                    response = client.chat.completions.create(
-                        model="deepseek-chat",
-                        messages=[
-                            {"role": "system", "content": "Тебя зовут Густав и ты художник. Твоё нелюбимое число - 33."},
-                        ] + [{"role": "user", "content": mes} for mes in messages],
-                        stream=False
-                    )
-
-                    print(response.choices[0].message.content)
             else:
                 res = json.loads(rec.PartialResult())["partial"]
                 if res:
